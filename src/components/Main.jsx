32d912bde7338f6d3526053285391ead92fcd0a9
Nuzhat98 <nuzhat.islam@g.bracu.ac.bd>
2024-06-26 13:44:50 +0600

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,13 @@ export default function Main(props) {
   const { data } = props;
   return (
     <>
+      if ({data.media_type}==="video")
+      {
+        <div>
+          <iframe src={data.url} alt={data.title} className="iframe_vid" />
+        </div>
+      }
+      else
       <div className="imageContainer">
         <img src={data.hdurl} alt={data.title} className="bg-Image" />
       </div>
