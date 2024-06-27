fec7c9ef1fc8d09af1ea3d7fd16f344f1cf0c054
Nuzhat98 <nuzhat.islam@g.bracu.ac.bd>
2024-06-28 01:25:18 +0600

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 export default function Main(props) {
   const { data } = props;
-  return (
-    <>
-      if ({data.media_type}==="video")
-      {
+
+  if (data.media_type == "video")
+    return (
+      <>
         <div>
           <iframe src={data.url} alt={data.title} className="iframe_vid" />
         </div>
-      }
-      else
-      <div className="imageContainer">
-        <img src={data.hdurl} alt={data.title} className="bg-Image" />
-      </div>
-    </>
-  );
+      </>
+    );
+  else if (data.media_type == "image")
+    return (
+      <>
+        <div className="imageContainer">
+          <img src={data.hdurl} alt={data.title} className="bg-Image" />
+        </div>
+      </>
+    );
 }
